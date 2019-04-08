1dcd2e4ae7b3950be0cf8f29413d2dc5359d2c5a
Manvir Brar <manvirbrar@Manvirs-MacBook-Pro.local>
2019-04-09 00:45:48 +0100

    test

--- test.groovy
+++ test.groovy
@@ -3,13 +3,15 @@
 def commits = ['mani','3mmi-m','3mmi-me','3mmi-m3','73mmi-m','73mmi-mer','73mmi-tygm'].unique()
 
 
-def call(Integer range = 5 ,def regex = /.*/){
+def call(){
         
         // List commits = "git --no-pager log --pretty=format:'%an:%s'".execute().text.tokenize("\n").unique()
         (matches,nonMatches) = commits.split{ it =~ regex }
 
         def moreThanRange = "Plus ${ matches.size() - range } others..."
         def lessThanRange = "No further commits found."
+        def range = 5
+        def regex = /.*/
 
         matches = (matches.size() < range) ? (matches.size() == 0) ? matches : matches << lessThanRange : matches[0..range - 1] << moreThanRange
     
